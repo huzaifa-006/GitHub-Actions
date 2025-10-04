a99e597ed97f37a222ba2a5a70313d385d0b28d1
Huzaifa Shafiq <huzaifa.shafiq@icloud.com>
2025-10-04 10:07:54 +0500

    Initial commit: URL Shortener

---
+++ src/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, request, redirect
+
+app = Flask(__name__)
+urls = {}
+
+@app.route('/')
+def shorten():
+	original_url = request.args.get("url")
+	if not original_url:
+		return "Please provide aURL"
+	short_key = str(len(urls) + 1)
+	urls[short_key] = original_url
+	return f"Shortened URL: /{short_key}
+
+@app.route('/<key>')
+def redirect_url(key):
+	original_url = urls.get(key)
+	if original_url:
+		return redirect(original_url)
+	return "Invalid short url"
